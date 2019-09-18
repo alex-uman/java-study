07b8677df164f308869c1737889077d45a5e2ce4
AUMA119 <AUMA119@PC07144496.cc-college.gecs.loc>
2019-09-18 15:53:28 +0200

    1

--- HelloEclipse/src/p1/NochEineKlasse.java
+++
@@ -1,9 +0,0 @@
-package p1;
-
-public class NochEineKlasse {
-
-	public static void main(String[] args) {
-	
-		System.out.println("one more text");
-	}
-}
--- HelloEclipse/src/variablen/probe.java
+++ HelloEclipse/src/variablen/probe.java
@@ -4,26 +4,65 @@ public class probe {
 
 	public static void main(String[] args) {
 		
-		double a = 0x9D;
-		//System.out.println(a);
+//		m1:
+//			
+//		for(int i=0;i<10;i++) {
+//			
+//			for(int j=0;j<3;j++) {
+//				
+//				if(i>j) {
+//					
+//					System.out.println("x"); continue m1;
+//					
+//				}
+//				
+//				System.out.println(j);
+//				
+//			}
+//			
+//			System.out.println("fin "+i);
+//			
+//		}
+//		
+//		System.out.println("ultima fin");
+		
+		
+		
+//		int i=1;
+//		
+//		switch (i) {
+//		
+//		case 1:
+//			System.out.println(i);
+//			
+//		case 2:
+//			System.out.println(i);
+//		
+//		}
 
-		double var1 = 1;
-		double var2 = var1++;
-		//System.out.println(var2);
+		int day=2;
 		
-		int x = 1;
+//		if(day<6 && day>0) System.out.println("3.A");
+//		
+//		else if (day==6||day==7) System.out.println("3.W");
+//		
+//		else System.out.println("3.???");
 		
-		x = x++ + 0;
 		
-		String c = "cd";
+		switch (day) {
 		
-		String s = false ? "yes":"no";
+		case 1: 
+		case 2: 
+		case 3: 
+		case 4: 
+		case 5: System.out.println("3.A"); break;
+		case 6: 
+		case 7: System.out.println("3.W"); break;
+		default: System.out.println("3.???");
+			
+		}
 		
-		double q = true ? var1:var2;
 		
-		System.out.println(q);
-		
-
 	}
 
 }
