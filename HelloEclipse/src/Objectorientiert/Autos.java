f41d85e5ac73c9ae049a6ed8c729dfd4ab0236a5
AUMA119 <AUMA119@PC07144496.cc-college.gecs.loc>
2019-09-24 13:36:09 +0200

    # WARNING: head commit changed in the meantime
    a

---
+++ HelloEclipse/src/Objectorientiert/Autos.java
@@ -0,0 +1,41 @@
+package Objectorientiert;
+
+class Auto {
+
+	static void printAuto(Auto aX) {
+
+		System.out.println((aX.modell == null ? "Dieses Auto" : aX.modell) + " fährt "
+				+ (aX.speed == 0 ? "nicht!" : (aX.speed + " Km/St.")));
+
+	}
+
+	String modell;
+	int speed;
+
+	void printAuto() {
+
+		System.out.println(
+				(modell == null ? "Dieses Auto" : modell) + " fährt " + (speed == 0 ? "nicht!" : (speed + " Km/St.")));
+
+	}
+
+}
+
+public class Autos {
+
+	public static void main(String[] args) {
+
+		Auto ford = new Auto();
+
+		ford.printAuto();
+
+		ford.modell = "Ford Ka";
+		ford.speed = 106;
+
+		System.out.println();
+
+		Auto.printAuto(ford);
+
+	}
+
+}
