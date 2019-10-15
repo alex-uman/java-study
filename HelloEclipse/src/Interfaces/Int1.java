d4131b6a0a6af684cb082202187d2f451ba015b5
alex-uman <55236184+alex-uman@users.noreply.github.com>
2019-10-15 08:03:46 +0200

    i

---
+++ HelloEclipse/src/Interfaces/Int1.java
@@ -0,0 +1,75 @@
+package Interfaces;
+
+interface Figur {
+
+	int getValue();
+
+}
+
+class Size implements Figur {
+
+	private int size;
+
+	Size(int size) {
+		this.size = size;
+	}
+
+	public int getValue() {
+		return size;
+	}
+
+	public String toString() {
+		return "Size = " + size;
+	}
+
+}
+
+class Kreis implements Figur {
+
+	private int radius;
+
+	Kreis(int radius) {
+		this.radius = radius;
+	}
+
+	public int getValue() {
+		return radius;
+	}
+
+	public String toString() {
+		return "Kreis. Radius = " + radius;
+	}
+
+}
+
+public class Int1 {
+
+	public static void main(String[] args) {
+
+//		Size s1 = new Size(20);
+//
+//		System.out.println("Size = " + s1);
+//
+//		Kreis k1 = new Kreis(3);
+//
+//		System.out.println(k1);
+
+		Size sA = new Size(12);
+		Size sB = new Size(7);
+
+		System.out.println(getMax(sA, sB));
+
+		Kreis kA = new Kreis(4);
+		Kreis kB = new Kreis(77);
+
+		System.out.println(getMax(kA, kB));
+
+	}
+
+	static String getMax(Figur A, Figur B) {
+
+		return A.getValue() > B.getValue() ? A.toString() : B.toString();
+
+	}
+
+}
