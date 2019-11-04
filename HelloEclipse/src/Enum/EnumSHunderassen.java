2dfc25f4aaa6085f1e57b5b2821e56ba239d5f28
alex-uman <55236184+alex-uman@users.noreply.github.com>
2019-11-04 15:05:51 +0100

    e

---
+++ HelloEclipse/src/Enum/EnumSHunderassen.java
@@ -0,0 +1,46 @@
+package Enum;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class EnumSHunderassen {
+
+	enum Hunderasse {
+
+		DACKEL(0.5) {
+			public String getRasse() {
+				return "Dackel";
+			}
+		},
+		COLLIE(1) {
+			public String getRasse() {
+				return "Collie";
+			}
+		},
+		DOGGE(1.5);
+
+		private double size;
+
+		Hunderasse(double size) {
+			this.size = size;
+		}
+
+		public String getRasse() {
+			return "Dogge";
+		}
+
+		public String toString() {
+			return getRasse() + ", max. Größe: " + size;
+		}
+
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(Hunderasse.DACKEL);
+		System.out.println(Hunderasse.COLLIE);
+		System.out.println(Hunderasse.DOGGE);
+
+	}
+}
